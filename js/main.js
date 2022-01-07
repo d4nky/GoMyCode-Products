a0c445fb4a46aa0f8cfb758a95ccd58343d3aab1
d4nky <vertix08@gmail.com>
2022-01-07 16:34:20 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var names = ['Stack of money', 'Island', 'PC', 'Pair of jordans', 'My will to live', 'Some Yasuo skin']
 
 var prices = [100, 200000000, 1000, 300, 0, 15]
+var descriptions = ['ouuuu money :)', 'Epstein style (yes offensive jokes, not for the faint hearted)', 'Latest specs desktop computer', 'Self explanatory', 'yes yes', 'Yasuo > literally every lol legends']
 var products = []
 
 var display = ''
@@ -8,26 +9,30 @@ var display = ''
 for (let i = 0; i < names.length; i++) {
     const name = names[i];
     const price = prices[i]
+    const description = descriptions[i];
     
     var product = {
         id: i,
         name: name,
         price: price,
+        description: description,
         quantity: 1
     }
 
     products.push(product);
 
     display += `<div class="col-4">
-            <div class="card" style="width: 18rem;">
-    <div class="card-body">
-    <h5 class="card-title">${name}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Get scammed</a>
-  </div>
-  <p class="card-text">${price}$</p>
-  </div>
-  </div>`
+                <div class="card mb-4 box-shadow" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${name}</h5>
+                        <p class="card-text">${description}.</p>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <a href="#" class="btn btn-primary">Get scammed</a>
+                        <p class="font-weight-bold">${price}$</p>
+                    </div>
+                </div>
+            </div>`
 }
 var container = document.querySelector('.row')
 container.insertAdjacentHTML('afterbegin', display)
