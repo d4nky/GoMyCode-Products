6e8d8e2aacc5375c7232ba6b6f16b97102e0f452
d4nky <vertix08@gmail.com>
2022-01-07 17:32:43 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var names = ['Stack of money', 'Island', 'PC', 'Pair of jordans', 'My will to live', 'Some Yasuo skin']
 
-var prices = [100, 200000000, 1000, 300, 0, 15]
-var descriptions = ['ouuuu money :)', 'Epstein style (yes offensive jokes, not for the faint hearted)', 'Latest specs desktop computer', 'Self explanatory', 'yes yes', 'Yasuo > literally every lol legends']
+var prices = [100, 200000000, 2500, 300, 0, 15]
+var descriptions = ['ouuuu money :)', 'Epstein style (yes offensive jokes, not for the faint hearted)', 'Latest specs desktop computer', 'Self explanatory', 'yes yes', 'Yasuo > literally every lol legend']
 var products = []
 
 var display = ''
@@ -21,14 +21,14 @@ for (let i = 0; i < names.length; i++) {
 
     products.push(product);
 
-    display += `<div class="col-4">
+    display += `<div class="col-5">
                 <div class="card mb-4 box-shadow" style="width: 18rem;">
-                    <div class="card-body">
+                    <div class="card-body p-4">
                         <h5 class="card-title">${name}</h5>
                         <p class="card-text">${description}.</p>
                     </div>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <a href="#" class="btn btn-primary">Get scammed</a>
+                    <div class="d-flex justify-content-between align-items-center p-4">
+                        <a id="addItem" href="#" class="btn btn-primary">Get scammed</a>
                         <p class="font-weight-bold">${price}$</p>
                     </div>
                 </div>
@@ -36,11 +36,15 @@ for (let i = 0; i < names.length; i++) {
 }
 var container = document.querySelector('.row')
 container.insertAdjacentHTML('afterbegin', display)
-
+var addBtn = document.querySelectorAll('addItem')
+var testDiv = document.getElementById(test)
 
 var cart = document.getElementById('cart')
 var cartList = []
+for (let i = 0; i < addBtn.length; i++) {
+    const clickedAddBtn = addBtn[i];
+    clickedAddBtn.addEventListener('click', (e) =>{
+        console.log('works')
+    })
+}
 
-cart.addEventListener('click', (e) =>{
-
-})
